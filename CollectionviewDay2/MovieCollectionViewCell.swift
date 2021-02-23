6e6bbdeb84d7d8e0b76f6abe1600e0422384ab21
tanvirgeekios <tanvir.alam@iosventure.com>
2021-02-23 16:00:01 +0600

    Collection view Grid, horizontal working

---
+++ CollectionviewDay2/MovieCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.swift
+//  CollectionviewDay2
+//
+//  Created by MD Tanvir Alam on 23/2/21.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var movieLabel: UILabel!
+}
--- CollectionviewDay2/ViewController.swift
+++ CollectionviewDay2/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    let myMovie = ["vegetables1", "vegetables2","vegetables1", "vegetables2","vegetables1", "vegetables2"]
+    
+    @IBOutlet weak var movieCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        movieCollectionView.delegate = self
+        movieCollectionView.dataSource = self
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        movieCollectionView.collectionViewLayout = layout
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return myMovie.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCollectionViewCell", for: indexPath) as! MovieCollectionViewCell
+        cell.movieImage.image = UIImage(named: myMovie[indexPath.row])
+        cell.movieLabel.text = myMovie[indexPath.row]
+        
+        return cell
     }
 
-
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 500, height: 300)
+    }
 }
 
